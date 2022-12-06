ea9f49e4cd69ef8b407797151a3a4f07bf49c031
Berg Jefferson <berg@degate.com>
2022-12-06 17:33:32 +0800

    remove redundant code

--- packages/hebao_v1/migrations/5_registries.js
+++
@@ -1,7 +0,0 @@
-const ModuleRegistryImpl = artifacts.require("ModuleRegistryImpl");
-
-module.exports = function(deployer) {
-  deployer.then(() => {
-    return Promise.all([deployer.deploy(ModuleRegistryImpl)]);
-  });
-};
